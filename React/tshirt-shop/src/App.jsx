8fb15232bc7c04655770a4a2e45a98e40045ade0
pablo.leite <pabloleite03@gmail.com>
2024-06-20 09:49:07 -0300

    feat: added tshirt shop project on the React folder

---
+++ React/tshirt-shop/src/App.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import "./App.css";
+import Item from "./components/Item";
+import OrderDetails from "./components/OrderDetails";
+
+function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      photo: "real_madrid.webp",
+      name: "Real Madrid",
+      price: 119.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 2,
+      photo: "milan.png",
+      name: "Milan",
+      price: 99.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 3,
+      photo: "chelsea.webp",
+      name: "Chelsea",
+      price: 99.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 4,
+      photo: "barcelona.png",
+      name: "Barcelona",
+      price: 109.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 5,
+      photo: "benfica.png",
+      name: "Benfica",
+      price: 89.49,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 6,
+      photo: "manchester.webp",
+      name: "Manchester City",
+      price: 129.79,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 7,
+      photo: "bayern.webp",
+      name: "Bayern",
+      price: 119.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 8,
+      photo: "psg.png",
+      name: "PSG",
+      price: 94.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 9,
+      photo: "ajax.webp",
+      name: "Ajax",
+      price: 89.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+  ]);
+
+  const itemsInBag = items.filter((item) => item.isInBag);
+
+  function selectProduct(id) {
+    let item = items.filter((item) => item.id === id)[0];
+    item.isInBag = !item.isInBag;
+    setItems(items.map((i) => (i.id === id ? item : i)));
+  }
+
+  function quantityHandler(e, id, increment) {
+    e.stopPropagation();
+    let item = items.filter((item) => item.id === id)[0];
+    item.quantity += increment;
+    setItems(items.map((i) => (i.id === id ? item : i)));
+  }
+
+  return (
+    <>
+      <section className="items">
+        <h4>Jersey Shop Made with React JS</h4>
+
+        {items.map((item) => (
+          <Item
+            selectProduct={(id) => selectProduct(id)}
+            changeQuantity={(e, id, increment) =>
+              quantityHandler(e, id, increment)
+            }
+            key={item.id}
+            item={item}
+          />
+        ))}
+      </section>
+
+      {itemsInBag.length > 0 && <OrderDetails itemsInBag={itemsInBag} />}
+    </>
+  );
+}
+
+export default App;
---
+++ React/tshirt-shop/src/components/OrderDetails.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function OrderDetails({ itemsInBag }) {
+  function calculateTotal() {
+    let total = 0;
+    itemsInBag.forEach((item) => (total += item.price * item.quantity));
+    return total.toFixed(2);
+  }
+
+  return (
+    <>
+      <section className="summary">
+        <strong>Order Details</strong>
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemsInBag.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  {item.quantity}x {item.name}
+                </td>
+                <td>$ {(item.price * item.quantity).toFixed(2)}</td>
+              </tr>
+            ))}
+
+            <tr>
+              <th>Total</th>
+              <th>$ {calculateTotal()}</th>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
+}
